81aa2864bc08eac0d432b3ec6e0df48933849419
Edward Yao <edw4rdyao@gmail.com>
2024-08-04 22:54:49 +0800

    Unifying the structure of models and model testing (#1)
    * feat: add data from dgl && unifying model test function
    
    * fix: remove .idea/ folder in git cache

---
+++ README.md
@@ -0,0 +1,23 @@
+# hugegraph-integrate-model-test
+
+## Run
+
+```commandline
+python main.py
+```
+
+## Model test result
+
+### DGI
+
+|    Dataset    | Cora | Citeseer | Pubmed |
+|:-------------:|:----:|:--------:|:------:|
+|     paper     | 82.3 |   71.8   |  76.8  |
+|     this      | 81.3 |   70.4   |  77.3  |
+
+### GRACE
+
+|    Dataset    | Cora | Citeseer | Pubmed |
+|:-------------:|:----:|:--------:|:------:|
+|     paper     | 83.3 |   72.1   |  86.7  |
+|     this      | 82.8 |   72.5   |  86.2  |
--- tests/dgi_test.py
+++ tests/dgi_test.py
@@ -1,89 +1,74 @@
-import argparse
-import os
 import time
 
 import dgl
 import torch
 import torch.nn.functional as F
-from dgl.data import load_data, register_data_args
 
+from dataset.dataset_from_dgl import dataset_from_dgl
 from models import Classifier, DGI
 
 
-def evaluate(model, features, labels, mask):
-    model.eval()
-    with torch.no_grad():
-        logits = model(features)
-        logits = logits[mask]
-        labels = labels[mask]
-        _, indices = torch.max(logits, dim=1)
-        correct = torch.sum(indices == labels)
-        return correct.item() * 1.0 / len(labels)
+def dgi_test(
+        dataset_name,
+        gpu=0,
+        self_loop=False,
+        n_hidden=512,
+        n_layers=2,
+        p_drop=0.0,
+        dgi_lr=1e-3,
+        clf_lr=1e-2,
+        n_dgi_epochs=300,
+        n_clf_epochs=300,
+        patience=30,
+        weight_decay=0.0,
+        verbose=0
+):
+    dataset = dataset_from_dgl(dataset_name)
+    graph = dataset[0]
+    feats = torch.FloatTensor(graph.ndata["feat"])
+    labels = torch.LongTensor(graph.ndata["label"])
+    train_mask = torch.BoolTensor(graph.ndata["train_mask"])
+    test_mask = torch.BoolTensor(graph.ndata["test_mask"])
+    n_in_feats = feats.shape[1]
+    n_classes = dataset.num_classes
 
+    if self_loop:
+        graph = dgl.remove_self_loop(graph)
+        graph = dgl.add_self_loop(graph)
 
-def main(args):
-    # load and preprocess dataset
-    data = load_data(args)
-    g = data[0]
-    features = torch.FloatTensor(g.ndata["feat"])
-    labels = torch.LongTensor(g.ndata["label"])
-    if hasattr(torch, "BoolTensor"):
-        train_mask = torch.BoolTensor(g.ndata["train_mask"])
-        val_mask = torch.BoolTensor(g.ndata["val_mask"])
-        test_mask = torch.BoolTensor(g.ndata["test_mask"])
-    else:
-        train_mask = torch.ByteTensor(g.ndata["train_mask"])
-        val_mask = torch.ByteTensor(g.ndata["val_mask"])
-        test_mask = torch.ByteTensor(g.ndata["test_mask"])
-    in_feats = features.shape[1]
-    n_classes = data.num_classes
-
-    if args.gpu < 0:
+    if gpu < 0:
         cuda = False
     else:
         cuda = True
-        torch.cuda.set_device(args.gpu)
-        features = features.cuda()
+        torch.cuda.set_device(gpu)
+        feats = feats.cuda()
         labels = labels.cuda()
         train_mask = train_mask.cuda()
-        val_mask = val_mask.cuda()
         test_mask = test_mask.cuda()
-
-    # add self loop
-    if args.self_loop:
-        g = dgl.remove_self_loop(g)
-        g = dgl.add_self_loop(g)
-    n_edges = g.num_edges()
-
-    if args.gpu >= 0:
-        g = g.to(args.gpu)
-    # create DGI model
+        graph = graph.to(gpu)
     dgi = DGI(
-        in_feats,
-        args.n_hidden,
-        args.n_layers,
-        args.dropout,
+        n_in_feats=n_in_feats,
+        n_hidden=n_hidden,
+        n_layers=n_layers,
+        p_drop=p_drop
     )
-
     if cuda:
         dgi.cuda()
-
     dgi_optimizer = torch.optim.Adam(
-        dgi.parameters(), lr=args.dgi_lr, weight_decay=args.weight_decay
+        dgi.parameters(), lr=dgi_lr, weight_decay=weight_decay
     )
-
     # train deep graph infomax
     cnt_wait = 0
     best = 1e9
     best_t = 0
     mean = 0
-    for epoch in range(args.n_dgi_epochs):
+    for epoch in range(n_dgi_epochs):
         dgi.train()
         if epoch >= 3:
             t0 = time.time()
 
         dgi_optimizer.zero_grad()
-        loss = dgi(g, features)
+        loss = dgi(graph, feats)
         loss.backward()
         dgi_optimizer.step()
 
@@ -91,84 +76,55 @@ def main(args):
             best = loss
             best_t = epoch
             cnt_wait = 0
-            torch.save(dgi.state_dict(), "best_dgi.pkl")
+            torch.save(dgi.state_dict(), "tmp_best_dgi.pkl")
         else:
             cnt_wait += 1
 
-        if cnt_wait == args.patience:
+        if cnt_wait == patience:
             print("Early stopping!")
             break
 
-        if epoch >= 3:
+        if epoch >= 3 and verbose > 0:
             mean = (mean * (epoch - 3) + (time.time() - t0)) / (epoch - 2)
-
-            print(
-                "Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f}".format(
-                    epoch, mean, loss.item()
-                )
-            )
+            print("Epoch {:04d} | Time(s) {:.4f} | Loss {:.4f}".format(epoch, mean, loss.item()))
 
     # create classifier model
-    classifier = Classifier(args.n_hidden, n_classes)
+    clf = Classifier(n_hidden, n_classes)
     if cuda:
-        classifier.cuda()
+        clf.cuda()
 
     classifier_optimizer = torch.optim.Adam(
-        classifier.parameters(),
-        lr=args.classifier_lr,
-        weight_decay=args.weight_decay,
+        clf.parameters(),
+        lr=clf_lr,
+        weight_decay=weight_decay,
     )
 
     # train classifier
     print("Loading {}th epoch".format(best_t))
-    dgi.load_state_dict(torch.load("best_dgi.pkl"))
-    embeds = dgi.encoder(g, features, corrupt=False)
+    dgi.load_state_dict(torch.load("tmp_best_dgi.pkl"))
+    embeds = dgi.encoder(graph, feats, corrupt=False)
     embeds = embeds.detach()
     mean = 0
-    for epoch in range(args.n_classifier_epochs):
-        classifier.train()
+    for epoch in range(n_clf_epochs):
+        clf.train()
         if epoch >= 3:
             t0 = time.time()
 
         classifier_optimizer.zero_grad()
-        preds = classifier(embeds)
+        preds = clf(embeds)
         loss = F.nll_loss(preds[train_mask], labels[train_mask])
         loss.backward()
         classifier_optimizer.step()
 
-        if epoch >= 3:
+        if epoch >= 3 and verbose > 0:
             mean = (mean * (epoch - 3) + (time.time() - t0)) / (epoch - 2)
+            print("Epoch {:04d} | Time(s) {:.4f} | Loss {:.4f}".format(epoch, mean, loss.item()))
 
-            acc = evaluate(classifier, embeds, labels, val_mask)
-            print(
-                "Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f} | Accuracy {:.4f}".format(
-                    epoch,
-                    mean,
-                    loss.item(),
-                    acc
-                )
-            )
+    clf.eval()
+    with torch.no_grad():
+        logits = clf(embeds)[test_mask]
+        _, predicted = torch.max(logits, dim=1)
+        correct = (predicted == labels[test_mask]).sum().item()
+        acc = correct / test_mask.sum().item()
 
-    print()
-    acc = evaluate(classifier, embeds, labels, test_mask)
-    print("Test Accuracy {:.4f}".format(acc))
-
-
-def dgi_test(test_args):
-    parser = argparse.ArgumentParser(description="DGI")
-    register_data_args(parser)
-    parser.add_argument("--dropout", type=float, default=0.0, help="dropout probability")
-    parser.add_argument("--gpu", type=int, default=-1, help="gpu")
-    parser.add_argument("--dgi-lr", type=float, default=1e-3, help="dgi learning rate")
-    parser.add_argument("--classifier-lr", type=float, default=1e-2, help="classifier learning rate")
-    parser.add_argument("--n-dgi-epochs", type=int, default=300, help="number of training epochs")
-    parser.add_argument("--n-classifier-epochs", type=int, default=300, help="number of training epochs")
-    parser.add_argument("--n-hidden", type=int, default=512, help="number of hidden gcn units")
-    parser.add_argument("--n-layers", type=int, default=2, help="number of gcn layers")
-    parser.add_argument("--weight-decay", type=float, default=0.0, help="Weight for L2 loss")
-    parser.add_argument("--patience", type=int, default=20, help="early stop patience condition")
-    parser.add_argument("--self-loop", action="store_true", help="graph self-loop (default=False)")
-    parser.set_defaults(self_loop=False)
-
-    args = parser.parse_args(test_args)
-    main(args)
+    print("dgi: dataset {} test accuracy {:.4f}".format(dataset_name, acc))
