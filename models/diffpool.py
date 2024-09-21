670e36c77b31d44e0e34422139b86d0ff5c41086
edw4rdyao <1697661410@qq.com>
2024-09-22 00:43:11 +0800

    feat: adjust diffpool model

--- models/diffpool.py
+++ models/diffpool.py
@@ -86,8 +86,7 @@ class DiffPool(nn.Module):
             )
         )
 
-        assign_dims = []
-        assign_dims.append(self.assign_dim)
+        assign_dims = [self.assign_dim]
         if self.concat:
             # diffpool layer receive pool_embedding_dim node feature tensor
             # and return pool_embedding_dim node embedding
@@ -95,7 +94,7 @@ class DiffPool(nn.Module):
         else:
             pool_embedding_dim = n_embedding
 
-        self.first_diffpool_layer = DiffPoolBatchedGraphLayer(
+        self.first_diffpool_layer = _DiffPoolBatchedGraphLayer(
             pool_embedding_dim,
             self.assign_dim,
             n_hidden,
@@ -107,15 +106,15 @@ class DiffPool(nn.Module):
 
         gc_after_per_pool = nn.ModuleList()
         for _ in range(n_layers - 1):
-            gc_after_per_pool.append(BatchedGraphSAGE(n_hidden, n_hidden))
-        gc_after_per_pool.append(BatchedGraphSAGE(n_hidden, n_embedding))
+            gc_after_per_pool.append(_BatchedGraphSAGE(n_hidden, n_hidden))
+        gc_after_per_pool.append(_BatchedGraphSAGE(n_hidden, n_embedding))
         self.gc_after_pool.append(gc_after_per_pool)
 
         self.assign_dim = int(self.assign_dim * pool_ratio)
         # each pooling module
         for _ in range(n_pooling - 1):
             self.diffpool_layers.append(
-                BatchedDiffPool(
+                _BatchedDiffPool(
                     pool_embedding_dim,
                     self.assign_dim,
                     n_hidden,
@@ -125,10 +124,10 @@ class DiffPool(nn.Module):
             gc_after_per_pool = nn.ModuleList()
             for _ in range(n_layers - 1):
                 gc_after_per_pool.append(
-                    BatchedGraphSAGE(n_hidden, n_hidden)
+                    _BatchedGraphSAGE(n_hidden, n_hidden)
                 )
             gc_after_per_pool.append(
-                BatchedGraphSAGE(n_hidden, n_embedding)
+                _BatchedGraphSAGE(n_hidden, n_embedding)
             )
             self.gc_after_pool.append(gc_after_per_pool)
             assign_dims.append(self.assign_dim)
@@ -152,33 +151,6 @@ class DiffPool(nn.Module):
                 if m.bias is not None:
                     m.bias.data = init.constant_(m.bias.data, 0.0)
 
-    def gcn_forward(self, g, h, gc_layers, cat=False):
-        """
-        Return gc_layer embedding cat.
-        """
-        block_readout = []
-        for gc_layer in gc_layers[:-1]:
-            h = gc_layer(g, h)
-            block_readout.append(h)
-        h = gc_layers[-1](g, h)
-        block_readout.append(h)
-        if cat:
-            block = torch.cat(block_readout, dim=1)  # N x F, F = F1 + F2 + ...
-        else:
-            block = h
-        return block
-
-    def gcn_forward_tensorized(self, h, adj, gc_layers, cat=False):
-        block_readout = []
-        for gc_layer in gc_layers:
-            h = gc_layer(h, adj)
-            block_readout.append(h)
-        if cat:
-            block = torch.cat(block_readout, dim=2)  # N x F, F = F1 + F2 + ...
-        else:
-            block = h
-        return block
-
     def forward(self, g, feat):
         self.link_pred_loss = []
         self.entropy_loss = []
@@ -189,7 +161,7 @@ class DiffPool(nn.Module):
         out_all = []
 
         # we use GCN blocks to get an embedding first
-        g_embedding = self.gcn_forward(g, h, self.gc_before_pool, self.concat)
+        g_embedding = _gcn_forward(g, h, self.gc_before_pool, self.concat)
 
         g.ndata["h"] = g_embedding
 
@@ -202,8 +174,8 @@ class DiffPool(nn.Module):
         adj, h = self.first_diffpool_layer(g, g_embedding)
         node_per_pool_graph = int(adj.size()[0] / len(g.batch_num_nodes()))
 
-        h, adj = batch2tensor(adj, h, node_per_pool_graph)
-        h = self.gcn_forward_tensorized(
+        h, adj = _batch2tensor(adj, h, node_per_pool_graph)
+        h = _gcn_forward_tensorized(
             h, adj, self.gc_after_pool[0], self.concat
         )
         readout = torch.sum(h, dim=1)
@@ -214,7 +186,7 @@ class DiffPool(nn.Module):
 
         for i, diffpool_layer in enumerate(self.diffpool_layers):
             h, adj = diffpool_layer(h, adj)
-            h = self.gcn_forward_tensorized(
+            h = _gcn_forward_tensorized(
                 h, adj, self.gc_after_pool[i + 1], self.concat
             )
             readout = torch.sum(h, dim=1)
@@ -243,51 +215,7 @@ class DiffPool(nn.Module):
                 loss += value
         return loss
 
-
-def batch2tensor(batch_adj, batch_feat, node_per_pool_graph):
-    """
-    transform a batched graph to batched adjacency tensor and node feature tensor
-    """
-    batch_size = int(batch_adj.size()[0] / node_per_pool_graph)
-    adj_list = []
-    feat_list = []
-    for i in range(batch_size):
-        start = i * node_per_pool_graph
-        end = (i + 1) * node_per_pool_graph
-        adj_list.append(batch_adj[start:end, start:end])
-        feat_list.append(batch_feat[start:end, :])
-    adj_list = list(map(lambda x: torch.unsqueeze(x, 0), adj_list))
-    feat_list = list(map(lambda x: torch.unsqueeze(x, 0), feat_list))
-    adj = torch.cat(adj_list, dim=0)
-    feat = torch.cat(feat_list, dim=0)
-
-    return feat, adj
-
-
-def masked_softmax(matrix, mask, dim=-1, memory_efficient=True, mask_fill_value=-1e32):
-    """
-    masked_softmax for dgl batch graph
-    code snippet contributed by AllenNLP (https://github.com/allenai/allennlp)
-    """
-    if mask is None:
-        result = torch.nn.functional.softmax(matrix, dim=dim)
-    else:
-        mask = mask.float()
-        while mask.dim() < matrix.dim():
-            mask = mask.unsqueeze(1)
-        if not memory_efficient:
-            result = torch.nn.functional.softmax(matrix * mask, dim=dim)
-            result = result * mask
-            result = result / (result.sum(dim=dim, keepdim=True) + 1e-13)
-        else:
-            masked_matrix = matrix.masked_fill(
-                (1 - mask).byte(), mask_fill_value
-            )
-            result = torch.nn.functional.softmax(masked_matrix, dim=dim)
-    return result
-
-
-class BatchedGraphSAGE(nn.Module):
+class _BatchedGraphSAGE(nn.Module):
     def __init__(
             self, infeat, outfeat, use_bn=True, mean=False, add_self=False
     ):
@@ -300,7 +228,6 @@ class BatchedGraphSAGE(nn.Module):
         nn.init.xavier_uniform_(
             self.W.weight, gain=nn.init.calculate_gain("relu")
         )
-
     def forward(self, x, adj):
         num_node_per_graph = adj.size(1)
         if self.use_bn and not hasattr(self, "bn"):
@@ -322,37 +249,33 @@ class BatchedGraphSAGE(nn.Module):
 
     def __repr__(self):
         if self.use_bn:
-            return "BN" + super(BatchedGraphSAGE, self).__repr__()
+            return "BN" + super(_BatchedGraphSAGE, self).__repr__()
         else:
-            return super(BatchedGraphSAGE, self).__repr__()
+            return super(_BatchedGraphSAGE, self).__repr__()
 
-
-class DiffPoolAssignment(nn.Module):
+class _DiffPoolAssignment(nn.Module):
     def __init__(self, nfeat, nnext):
         super().__init__()
-        self.assign_mat = BatchedGraphSAGE(nfeat, nnext, use_bn=True)
-
+        self.assign_mat = _BatchedGraphSAGE(nfeat, nnext, use_bn=True)
     def forward(self, x, adj, log=False):
         s_l_init = self.assign_mat(x, adj)
         s_l = F.softmax(s_l_init, dim=-1)
         return s_l
 
-
-class BatchedDiffPool(nn.Module):
+class _BatchedDiffPool(nn.Module):
     def __init__(self, nfeat, nnext, nhid, link_pred=False, entropy=True):
-        super(BatchedDiffPool, self).__init__()
+        super(_BatchedDiffPool, self).__init__()
         self.link_pred = link_pred
         self.log = {}
-        self.link_pred_layer = LinkPredLoss()
-        self.embed = BatchedGraphSAGE(nfeat, nhid, use_bn=True)
-        self.assign = DiffPoolAssignment(nfeat, nnext)
+        self.link_pred_layer = _LinkPredLoss()
+        self.embed = _BatchedGraphSAGE(nfeat, nhid, use_bn=True)
+        self.assign = _DiffPoolAssignment(nfeat, nnext)
         self.reg_loss = nn.ModuleList([])
         self.loss_log = {}
         if link_pred:
-            self.reg_loss.append(LinkPredLoss())
+            self.reg_loss.append(_LinkPredLoss())
         if entropy:
-            self.reg_loss.append(EntropyLoss())
-
+            self.reg_loss.append(_EntropyLoss())
     def forward(self, x, adj, log=False):
         z_l = self.embed(x, adj)
         s_l = self.assign(x, adj)
@@ -368,8 +291,7 @@ class BatchedDiffPool(nn.Module):
             self.log["a"] = anext.cpu().numpy()
         return xnext, anext
 
-
-class DiffPoolBatchedGraphLayer(nn.Module):
+class _DiffPoolBatchedGraphLayer(nn.Module):
     def __init__(
             self,
             input_dim,
@@ -380,7 +302,7 @@ class DiffPoolBatchedGraphLayer(nn.Module):
             aggregator_type,
             link_pred,
     ):
-        super(DiffPoolBatchedGraphLayer, self).__init__()
+        super(_DiffPoolBatchedGraphLayer, self).__init__()
         self.embedding_dim = input_dim
         self.assign_dim = assign_dim
         self.hidden_dim = output_feat_dim
@@ -401,7 +323,7 @@ class DiffPoolBatchedGraphLayer(nn.Module):
         )
         self.reg_loss = nn.ModuleList([])
         self.loss_log = {}
-        self.reg_loss.append(EntropyLoss())
+        self.reg_loss.append(_EntropyLoss())
 
     def forward(self, g, h):
         feat = self.feat_gc(
@@ -434,8 +356,7 @@ class DiffPoolBatchedGraphLayer(nn.Module):
 
         return adj_new, h
 
-
-class EntropyLoss(nn.Module):
+class _EntropyLoss(nn.Module):
     # Return Scalar
     def forward(self, adj, anext, s_l):
         entropy = (
@@ -446,11 +367,78 @@ class EntropyLoss(nn.Module):
         assert not torch.isnan(entropy)
         return entropy
 
-
-class LinkPredLoss(nn.Module):
+class _LinkPredLoss(nn.Module):
     def forward(self, adj, anext, s_l):
         link_pred_loss = (adj - s_l.matmul(s_l.transpose(-1, -2))).norm(
             dim=(1, 2)
         )
         link_pred_loss = link_pred_loss / (adj.size(1) * adj.size(2))
         return link_pred_loss.mean()
+
+def _batch2tensor(batch_adj, batch_feat, node_per_pool_graph):
+    """
+    transform a batched graph to batched adjacency tensor and node feature tensor
+    """
+    batch_size = int(batch_adj.size()[0] / node_per_pool_graph)
+    adj_list = []
+    feat_list = []
+    for i in range(batch_size):
+        start = i * node_per_pool_graph
+        end = (i + 1) * node_per_pool_graph
+        adj_list.append(batch_adj[start:end, start:end])
+        feat_list.append(batch_feat[start:end, :])
+    adj_list = list(map(lambda x: torch.unsqueeze(x, 0), adj_list))
+    feat_list = list(map(lambda x: torch.unsqueeze(x, 0), feat_list))
+    adj = torch.cat(adj_list, dim=0)
+    feat = torch.cat(feat_list, dim=0)
+
+    return feat, adj
+
+def _masked_softmax(matrix, mask, dim=-1, memory_efficient=True, mask_fill_value=-1e32):
+    """
+    masked_softmax for dgl batch graph
+    code snippet contributed by AllenNLP (https://github.com/allenai/allennlp)
+    """
+    if mask is None:
+        result = torch.nn.functional.softmax(matrix, dim=dim)
+    else:
+        mask = mask.float()
+        while mask.dim() < matrix.dim():
+            mask = mask.unsqueeze(1)
+        if not memory_efficient:
+            result = torch.nn.functional.softmax(matrix * mask, dim=dim)
+            result = result * mask
+            result = result / (result.sum(dim=dim, keepdim=True) + 1e-13)
+        else:
+            masked_matrix = matrix.masked_fill(
+                (1 - mask).byte(), mask_fill_value
+            )
+            result = torch.nn.functional.softmax(masked_matrix, dim=dim)
+    return result
+
+def _gcn_forward(g, h, gc_layers, cat=False):
+    """
+    Return gc_layer embedding cat.
+    """
+    block_readout = []
+    for gc_layer in gc_layers[:-1]:
+        h = gc_layer(g, h)
+        block_readout.append(h)
+    h = gc_layers[-1](g, h)
+    block_readout.append(h)
+    if cat:
+        block = torch.cat(block_readout, dim=1)  # N x F, F = F1 + F2 + ...
+    else:
+        block = h
+    return block
+
+def _gcn_forward_tensorized(h, adj, gc_layers, cat=False):
+    block_readout = []
+    for gc_layer in gc_layers:
+        h = gc_layer(h, adj)
+        block_readout.append(h)
+    if cat:
+        block = torch.cat(block_readout, dim=2)  # N x F, F = F1 + F2 + ...
+    else:
+        block = h
+    return block
